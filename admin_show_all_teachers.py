6f745f143e29511220e0358bd1011cea2c2d8088
JoykishanSharma <joykishan120@gmail.com>
2021-12-09 16:38:13 +0530

    admin show all teacher - variable update

--- admin_show_all_teachers.py
+++ admin_show_all_teachers.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from tkinter import Tk, Canvas, PhotoImage, Frame, ttk, VERTICAL, HORIZONTAL, BOTTOM, RIGHT, X, Y, BOTH, StringVar, END, \
-    Button, messagebox
+from tkinter import Tk, Canvas, PhotoImage, Frame, ttk, VERTICAL, HORIZONTAL, BOTTOM, \
+    RIGHT, X, Y, BOTH, StringVar, END, Button, messagebox
 import mysql.connector  # pip install mysql-connector-python
 
 import all_credentials
@@ -32,16 +32,16 @@ def fetch_data():
     cursor.execute("select * from teachers")
     data = cursor.fetchall()
     if len(data) != 0:
-        student_table.delete(*student_table.get_children())
+        teacher_table.delete(*teacher_table.get_children())
         for i in data:
-            student_table.insert("", END, values=i)
+            teacher_table.insert("", END, values=i)
         connection.commit()
     connection.close()
 
 
 def get_cursor(event=""):
-    cursor_row = student_table.focus()
-    content = student_table.item(cursor_row)
+    cursor_row = teacher_table.focus()
+    content = teacher_table.item(cursor_row)
     data = content["values"]
 
     teacher_id.set(data[0])
@@ -141,7 +141,7 @@ table_frame.place(x=51, y=112, width=852, height=335)
 
 scroll_bar_x = ttk.Scrollbar(table_frame, orient=HORIZONTAL)
 scroll_bar_y = ttk.Scrollbar(table_frame, orient=VERTICAL)
-student_table = ttk.Treeview(table_frame, column=("teacher_id",
+teacher_table = ttk.Treeview(table_frame, column=("teacher_id",
                                                   "teacher_name",
                                                   "teacher_email",
                                                   "teacher_mobile",
@@ -157,37 +157,37 @@ student_table = ttk.Treeview(table_frame, column=("teacher_id",
 scroll_bar_x.pack(side=BOTTOM, fill=X)
 scroll_bar_y.pack(side=RIGHT, fill=Y)
 
-scroll_bar_x.config(command=student_table.xview)
-scroll_bar_y.config(command=student_table.yview)
+scroll_bar_x.config(command=teacher_table.xview)
+scroll_bar_y.config(command=teacher_table.yview)
 
-student_table.heading("teacher_id", text="Teacher ID")
-student_table.heading("teacher_name", text="Teacher Name")
-student_table.heading("teacher_email", text="Teacher Email")
-student_table.heading("teacher_mobile", text="Teacher Mobile")
-student_table.heading("teacher_qualification", text="Teacher Qualification")
-student_table.heading("teaching_subject", text="Teaching Subject")
-student_table.heading("course_mentor", text="Course Mentor")
-student_table.heading("monthly_salary", text="monthly Salary")
-student_table.heading("gender", text="Gender")
-student_table.heading("age", text="age")
-student_table.heading("residential_address", text="Residential Address")
+teacher_table.heading("teacher_id", text="Teacher ID")
+teacher_table.heading("teacher_name", text="Teacher Name")
+teacher_table.heading("teacher_email", text="Teacher Email")
+teacher_table.heading("teacher_mobile", text="Teacher Mobile")
+teacher_table.heading("teacher_qualification", text="Teacher Qualification")
+teacher_table.heading("teaching_subject", text="Teaching Subject")
+teacher_table.heading("course_mentor", text="Course Mentor")
+teacher_table.heading("monthly_salary", text="monthly Salary")
+teacher_table.heading("gender", text="Gender")
+teacher_table.heading("age", text="age")
+teacher_table.heading("residential_address", text="Residential Address")
 
-student_table["show"] = "headings"
+teacher_table["show"] = "headings"
 
-student_table.column("teacher_id", width=100)
-student_table.column("teacher_name", width=150)
-student_table.column("teacher_email", width=200)
-student_table.column("teacher_mobile", width=150)
-student_table.column("teacher_qualification", width=200)
-student_table.column("teaching_subject", width=200)
-student_table.column("course_mentor", width=200)
-student_table.column("monthly_salary", width=100)
-student_table.column("gender", width=100)
-student_table.column("age", width=100)
-student_table.column("residential_address", width=500)
+teacher_table.column("teacher_id", width=100)
+teacher_table.column("teacher_name", width=150)
+teacher_table.column("teacher_email", width=200)
+teacher_table.column("teacher_mobile", width=150)
+teacher_table.column("teacher_qualification", width=200)
+teacher_table.column("teaching_subject", width=200)
+teacher_table.column("course_mentor", width=200)
+teacher_table.column("monthly_salary", width=100)
+teacher_table.column("gender", width=100)
+teacher_table.column("age", width=100)
+teacher_table.column("residential_address", width=500)
 
-student_table.pack(fill=BOTH, expand=1)
-student_table.bind("<ButtonRelease>", get_cursor)
+teacher_table.pack(fill=BOTH, expand=1)
+teacher_table.bind("<ButtonRelease>", get_cursor)
 fetch_data()
 
 window.resizable(False, False)
